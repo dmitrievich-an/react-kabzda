62b7ee78558cce473a2dd1394655510270f50c78
Artem <reebok001@yandex.ru>
2022-12-27 14:35:21 +0300

    end 2nd lesson + homework

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,18 @@ import './App.css';
 function App() {
   return (
     <div>
-      This is App component
+      <AppTitle />
       <Rating />
       <Accordion />
+      <Rating />
+
     </div>
   );
 }
+
+function AppTitle() {
+  return <>This is App component</>
+  }
 
 function Star() {
   return (
@@ -29,15 +35,25 @@ function Rating() {
   );
 }
 
+function AccordionTitle() {
+  return <h3>Menu</h3>
+}
+
+function AccordionBody() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+}
+
 function Accordion() {
   return (
     <div>
-      <h3>Menu</h3>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
+      <AccordionTitle />
+      <AccordionBody />
     </div>
   );
 }
